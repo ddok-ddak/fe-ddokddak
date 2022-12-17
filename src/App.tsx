a6a9fcfec74a7ad9529a10fc7930ab6eb2aee498
min <69198709+matchaing@users.noreply.github.com>
2022-12-17 14:39:39 +0900

    Create Router Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import SamplePage from './pages/SamplePage';
+import RecordPage from './pages/RecordPage';
+import StatisticsPage from './pages/StatisticsPage';
+import SettingsPage from './pages/SettingsPage';
 
 function App() {
   return (
     <div className="App">
-      <SamplePage/>
+      {/* <SamplePage /> */}
+      <Routes>
+        <Route path="/" element={<SamplePage />} />
+        <Route path="/record" element={<RecordPage />} />
+        <Route path="/statistics" element={<StatisticsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Routes>
     </div>
   );
 }
