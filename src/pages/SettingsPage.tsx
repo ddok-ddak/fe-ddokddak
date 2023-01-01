b49d41794f4d39fc7a41883c5d6759bee5a8c9cd
min <69198709+matchaing@users.noreply.github.com>
2023-01-02 08:47:30 +0900

    환경설정 페이지 작업

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,41 +1,33 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Button } from '@mui/material';
 import '../styles/setting.css';
-import { useRecoilState } from 'recoil';
+import '../styles/index';
 
 const SettingPage = () => {
-  // const [user, setUser] = useRecoilState();
   return (
     <div>
       <div className="box">
         <img className="profile"></img>
-        {/* <div className="username">`${user}` 회원님</div> */}
         <div className="username"> 00 회원님</div>
+        {/* <div className="arrow"></div> */}
       </div>
-
       <div className="items">
-        <ul className="items_ul">
-          <li>
-            <div className="item">커스텀화</div>
-          </li>
+        <ul>
+          <div className="item">커스텀화</div>
           <li>
             <a href="/">테마 설정</a>
           </li>
           <li>
             <a href="/">한 주의 시작 요일 설정</a>
           </li>
-          <li>
-            <div className="item">고객센터</div>
-          </li>
+          <div className="item">고객센터</div>
           <li>
             <a href="/">도움말</a>
           </li>
           <li>
             <a href="/">문의하기</a>
           </li>
-          <li>
-            <div className="item">기타</div>
-          </li>
+          <div className="item">기타</div>
           <li>
             <a href="/">로그아웃</a>
           </li>
