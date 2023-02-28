ba5ebf6cf0f4abf9fbe117ecc98b827183f39a75
min <69198709+matchaing@users.noreply.github.com>
2023-02-28 18:15:10 +0900

    로그인, 회원가입 페이지 라우터

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { ThemeProvider } from '@mui/material';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import BottomNav from '@/components/layout/BottomNav';
+import LoginPage from '@/pages/auth/Login';
+import SignUpPage from '@/pages/auth/SignUp';
 import CreateRecoredPage from '@/pages/record/CreateRecordPage';
 import RecordPage from '@/pages/record/RecordPage';
 import SettingsPage from '@/pages/settings/SettingsPage';
 import StatisticsPage from '@/pages/statistics/StatisticsPage';
+
 import { theme } from '@/styles';
 
 function App() {
@@ -14,7 +17,9 @@ function App() {
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <Routes>
-          <Route path="/" element={<Navigate to="/record" />} />
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signUp" element={<SignUpPage />} />
           <Route path="/record" element={<RecordPage />} />
           <Route path="/statistics" element={<StatisticsPage />} />
           <Route path="/settings" element={<SettingsPage />} />
