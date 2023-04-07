f14ca11daf105bcaf9ee084f57abaa3341404d6e
min <69198709+matchaing@users.noreply.github.com>
2023-04-07 11:43:47 +0900

    활동 기록하기 개발 중

--- src/pages/record/CreateRecordPage.tsx
+++ src/pages/record/CreateRecordPage.tsx
@@ -13,14 +13,17 @@ import {
 import { addDays } from 'date-fns';
 import { ReactElement, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useRecoilValue, useRecoilState, selectedDaysState  } from 'recoil';
+import { useRecoilValue, useRecoilState  } from 'recoil';
+import axios from 'axios';
 
 import Circle from '@/components/common/Circle';
 import FlexBox from '@/components/common/FlexBox';
 import DaysChip from '@/components/date/DaysChip';
 import CommonHeader from '@/components/layout/CommonHeader';
 import { MainCategory } from '@/pages/category/CategoryPage';
-import { recoilCategory, recoilSubCategory, selectedTimeRangeState } from '@/store/record';
+import { categoriesState, categories  , selectedDaysState, recoilCategory, recoilSubCategory, selectedTimeRangeState } from '@/store/record';
+import { useSetRecoilState } from 'recoil';
+// import {categoriesState, selectedTimeRangeState } from '@/store/record';
 
 // import styles from './CreateRecordPage.module.css';
 
@@ -29,75 +32,124 @@ export interface SelectedRangeData {
   end: Date;
 }
 
+// type SubCategory = {
+//   title: string;
+//   color: string;
+// };
+
+// type Category = {
+//   title: string;
+//   color: string;
+//   subCategories: SubCategory[];
+// };
 
 /**
  * 임시 data
  */
-const categories: MainCategory[] = [
-  {
-    title: '직장',
-    color: '#20FFD7',
-    subCategories: [
-      { title: '업무', color: '#C2FFF4' },
-      { title: '야근', color: '#C2FFF4' },
-      { title: '출장', color: '#C2FFF4' },
-      { title: '회식', color: '#C2FFF4' },
-    ],
-  },
-  {
-    title: '성장',
-    color: '#20FFD7',
-    subCategories: [
-      { title: '강의', color: '#C2FFF4' },
-      { title: '독서', color: '#C2FFF4' },
-      { title: '자격증', color: '#C2FFF4' },
-    ],
-  },
-  {
-    title: '관계',
-    color: '#20FFD7',
-    subCategories: [
-      { title: '가족', color: '#C2FFF4' },
-      { title: '연인', color: '#C2FFF4' },
-      { title: '친구', color: '#C2FFF4' },
-    ],
-  },
-  {
-    title: '건강',
-    color: '#20FFD7',
-    subCategories: [
-      { title: '식사', color: '#C2FFF4' },
-      { title: '운동', color: '#C2FFF4' },
-      { title: '잠', color: '#C2FFF4' },
-    ],
-  },
-  {
-    title: '낭비',
-    color: '#20FFD7',
-    subCategories: [
-      { title: 'SNS', color: '#C2FFF4' },
-      { title: '미디어', color: '#C2FFF4' },
-      { title: '멍', color: '#C2FFF4' },
-      { title: '웹서핑', color: '#C2FFF4' },
-    ],
-  },
-];
+// const categories: MainCategory[] = [
+//   {
+//     title: '직장',
+//     color: '#20FFD7',
+//     subCategories: [
+//       { title: '업무', color: '#C2FFF4' },
+//       { title: '야근', color: '#C2FFF4' },
+//       { title: '출장', color: '#C2FFF4' },
+//       { title: '회식', color: '#C2FFF4' },
+//     ],
+//   },
+//   {
+//     title: '성장',
+//     color: '#20FFD7',
+//     subCategories: [
+//       { title: '강의', color: '#C2FFF4' },
+//       { title: '독서', color: '#C2FFF4' },
+//       { title: '자격증', color: '#C2FFF4' },
+//     ],
+//   },
+//   {
+//     title: '관계',
+//     color: '#20FFD7',
+//     subCategories: [
+//       { title: '가족', color: '#C2FFF4' },
+//       { title: '연인', color: '#C2FFF4' },
+//       { title: '친구', color: '#C2FFF4' },
+//     ],
+//   },
+//   {
+//     title: '건강',
+//     color: '#20FFD7',
+//     subCategories: [
+//       { title: '식사', color: '#C2FFF4' },
+//       { title: '운동', color: '#C2FFF4' },
+//       { title: '잠', color: '#C2FFF4' },
+//     ],
+//   },
+//   {
+//     title: '낭비',
+//     color: '#20FFD7',
+//     subCategories: [
+//       { title: 'SNS', color: '#C2FFF4' },
+//       { title: '미디어', color: '#C2FFF4' },
+//       { title: '멍', color: '#C2FFF4' },
+//       { title: '웹서핑', color: '#C2FFF4' },
+//     ],
+//   },
+// ];
 
+// type SubCategory = {
+//   title: string;
+//   color: string;
+// };
+
+// type Category = {
+//   title: string;
+//   color: string;
+//   subCategories: SubCategory[];
+// };
+
+// const categoriesState = atom<Category[]>({
+//   key: 'categoriesState',
+//   default: [],
+// });
+  
 const getAMPM = (date: Date) => {
   const hours = date.getHours();
   const ampm = hours > 12 ? 'PM' : 'AM';
   return ampm;
 };
 
-const CreateRecoredPage  = (): ReactElement => {
+
+const CreateRecordPage= (): ReactElement => {
+  const [categories, setCategories] = useRecoilState(categoriesState);
+
   const selectedDate = useRecoilValue(selectedTimeRangeState);
-  const [selectedDays, setSelectedDays] = useState<number[]>([]);
+  const [selectedDays, setSelectedDays] = useRecoilState(selectedDaysState);
   const [selectedCategoryIdx, setSelectedCategoryIdx] = useState(0);
   const [selectedSubCategoryIdx, setSelectedSubCategoryIdx] = useState(0);
 
   const [recoilCategoryValue, setRecoilCategoryValue] = useRecoilState(recoilCategory);
   const [recoilSubCategoryValue, setRecoilSubCategoryValue] = useRecoilState(recoilSubCategory);
 
+
+  //카테고리 API 요청해서 렌더링
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const memberId = 1;
+      const res = await fetch(`http://localhost:8080/api/v1/categories?memberId=${encodeURIComponent(memberId)}`);
+      const data = await res.json();
+      setCategories(data.result);
+    }
+    fetchCategories();
+    
+  }, [setCategories]);
+
+  console.log("categories: ",categories);
+  //
+  useEffect(() => {
+    setSelectedSubCategoryIdx(0);
+  }, [selectedCategoryIdx]);
+
+  //선택한 메인카테고리, sub카테고리 recoil에 저장
   useEffect(() => {
     setSelectedSubCategoryIdx(0);
     setRecoilCategoryValue(selectedCategoryIdx);
@@ -108,18 +160,17 @@ const CreateRecoredPage  = (): ReactElement => {
   }, [selectedSubCategoryIdx, setRecoilSubCategoryValue]);
   console.log(recoilCategoryValue, recoilSubCategoryValue);
 
-  //날짜 형식 2023-01-01T13:00:00 KST으로 포멧
-const formatDate = (date: Date): string => {
+  // 날짜 형식 2023-01-01T13:00:00 KST으로 포맷
+  const formatDate = (date: Date): string => {
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
     const day = date.getDate();
     const hour = date.getHours();
     const minute = date.getMinutes();
     const second = date.getSeconds();
-  
+
     return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}T${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:${second.toString().padStart(2, '0')} KST`;
   };
-
   console.log('selectedDate.start: ',selectedDate.start);
   //Wed Mar 22 2023 07:00:00 GMT+0900 (한국 표준시)
   console.log('formatDate(selectedDate.start): ', formatDate(selectedDate.start));
@@ -127,56 +178,119 @@ const formatDate = (date: Date): string => {
   console.log('formatDate(selectedDate.end): ', formatDate(selectedDate.end));
 
   const navigate = useNavigate();
-  ////시간소비 활동 API - 등록
-  // const postData = async () => {
-  //   const res = await fetch('http://localhost:8080/api/v1/activity-records', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json;charset=UTF-8',
-  //       'Content-Length': '165',
-  //       'Host': 'localhost:8080',
-  //     },
-  //     body: JSON.stringify({
-  //       categoryId: 1,
-  //       startedAt: formatDate(selectedDate.start),
-  //       finishedAt: formatDate(selectedDate.end),
-  //       content: "test-activity",
-  //       timeUnit: 30,
-  //     }),
-  //   });
-  //   if (res.ok) {
-  //     navigate('/record'); // 페이지 이동
-  //   } else {
-  //     console.log('error');
-  //   }
-  //   const data = await res.json();
-  //   console.log(data);
-  // };
 
-  function removeKST(str: string): string {
-    return str.replace(/ KST$/, '');
-  }
-
-  ////로컬에 저장
-  const handleRecordCreate = (): void =>  {
-    const events = JSON.parse(localStorage.getItem('events') || '[]');
-    selectedDays.push(selectedDate.start.getDay());
-
-    // selectedDays에 있는 모든 요일에 대해 일정 추가
-    const newEvents = selectedDays.map((dayIndex) => ({
-      id: String(events.length + 1), // 기존 이벤트 수보다 1 증가
-      title: categories.find((category) => category.title === ['직장', '성장', '관계', '건강', '낭비'][recoilCategoryValue])?.title,
-      start: removeKST(formatDate(addDays(selectedDate.start, dayIndex))),
-      end: removeKST(formatDate(addDays(selectedDate.end, dayIndex)))
-    }));
   
-    // 기존 이벤트와 새로운 이벤트를 합쳐서 localStorage에 저장
-    localStorage.setItem('events', JSON.stringify([...events, ...newEvents]));
-    console.log(events);
-    navigate('/record');
-  };
+  ////시간소비 활동 API - 등록
+  async function postData(title: string, startedAt: string, finishedAt: string): Promise<{
+    categoryId: number;
+    title: string;
+    startedAt: string;
+    finishedAt: string;
+    timeUnit: number;
+  }> {
 
-  // 요일 여러개 선택
+    const newEvents = {
+      categoryId: selectedCategoryIdx,
+      title,
+      startedAt,
+      finishedAt,
+      timeUnit: 30,
+    };
+  
+    const res = await fetch("http://localhost:8080/api/v1/activity-records?memberId=1", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+        "Host": "localhost:8080",
+      },
+      body: JSON.stringify(newEvents),
+    });
+  
+    if (res.ok) {
+      setSelectedDays([]);
+      navigate("/record");
+    } else {
+      console.log("에러 발생");
+    }
+  
+    const data = await res.json();
+    return {
+      categoryId: data.categoryId,
+      title: data.title,
+      startedAt: data.startedAt,
+      finishedAt: data.finishedAt,
+      timeUnit: data.timeUnit,
+    };
+  };
+  /**
+   * 요일 여러개 선택 해서 post
+  **/
+
+  // const events: Event[] = [];
+  // async function postData(
+  //   title: string,
+  //   startedAt: string,
+  //   finishedAt: string
+  // ): Promise<{
+  //   id: number;
+  //   categoryId: number;
+  //   title: string;
+  //   startedAt: string;
+  //   finishedAt: string;
+  //   timeUnit: number;
+  // }[]> {
+  //   const newEvents = selectedDays.map((day) => {
+  //     const categoryId = categories[selectedCategoryIdx];
+  //     const eventCount = events.length + 1;
+  //     const id = eventCount;
+  //     return {
+  //       id,
+  //       categoryId,
+  //       title,
+  //       startedAt: formatDate(data.startedAt),
+  //       finishedAt: formatDate(data.finishedAt),
+  //       timeUnit: 30,
+  //     };
+  //   });
+  
+  //   const res = await fetch("http://localhost:8080/api/v1/activity-records?memberId=1", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json;charset=UTF-8",
+  //       Host: "localhost:8080",
+  //     },
+  //     body: JSON.stringify(newEvents),
+  //   });
+  
+  //   if (res.ok) {
+  //     setSelectedDays([]);
+  //     navigate("/record");
+  //   } else {
+  //     console.log("에러 발생");
+  //   }
+  
+  //   const data = await res.json();
+  //   return data.map((d: any) => ({
+  //     id: d.id,
+  //     categoryId: d.categoryId,
+  //     title: d.title,
+  //     startedAt: d.startedAt,
+  //     finishedAt: d.finishedAt,
+  //     timeUnit: d.timeUnit,
+  //   }));
+  // }
+
+  
+  console.log("categories: ", categories);
+  console.log("selectedCategoryIdx: ",selectedCategoryIdx);
+  console.log("categories[selectedCategoryIdx]?.subCategories: ", categories[selectedCategoryIdx]?.subCategories);
+
+  // function removeKST(str: string): string {
+  //   return str.replace(/ KST$/, '');
+  // }
+
+
+  // // // 요일 여러개 선택
   const handleDayChipClick = (dayIndex: number) => {
     if (selectedDays.includes(dayIndex)) {
       setSelectedDays(selectedDays.filter((selectedDay) => selectedDay !== dayIndex));
@@ -186,11 +300,15 @@ const formatDate = (date: Date): string => {
     return selectedDays;
   };
 
+  function handleRightButtonClick() {
+    const title = categories[selectedCategoryIdx].title;
+    const startedAt = formatDate(selectedDate.start);
+    const finishedAt = formatDate(selectedDate.end);
+    postData(title, startedAt, finishedAt);
+  }
   return (
     <>
-      <CommonHeader title={'기록하기'} isShowBackButton={true} isShowRightButton={true}
-      // onClickRightButton={postData}/>
-      onClickRightButton={handleRecordCreate}/>
+      <CommonHeader title={'기록하기'} isShowBackButton={true} isShowRightButton={true} onClickRightButton={handleRightButtonClick}/>
       <Container>
         <Container
           sx={{
@@ -199,13 +317,13 @@ const formatDate = (date: Date): string => {
             marginTop: '8px',
           }}
         >
+          <DaysChip title="일" isSelected={selectedDate.start.getDay() === 0 || selectedDays.includes(0)} onClick={() => handleDayChipClick(0)} />
           <DaysChip title="월" isSelected={selectedDate.start.getDay() === 1 || selectedDays.includes(1)} onClick={() => handleDayChipClick(1)} />
           <DaysChip title="화" isSelected={selectedDate.start.getDay() === 2 || selectedDays.includes(2)} onClick={() => handleDayChipClick(2)} />
           <DaysChip title="수" isSelected={selectedDate.start.getDay() === 3 || selectedDays.includes(3)} onClick={() => handleDayChipClick(3)} />
           <DaysChip title="목" isSelected={selectedDate.start.getDay() === 4 || selectedDays.includes(4)} onClick={() => handleDayChipClick(4)} />
           <DaysChip title="금" isSelected={selectedDate.start.getDay() === 5 || selectedDays.includes(5)} onClick={() => handleDayChipClick(5)} />
           <DaysChip title="토" isSelected={selectedDate.start.getDay() === 6 || selectedDays.includes(6)} onClick={() => handleDayChipClick(6)} />
-          <DaysChip title="일" isSelected={selectedDate.start.getDay() === 0 || selectedDays.includes(0)} onClick={() => handleDayChipClick(0)} />
 
         </Container>
         <Container
@@ -236,8 +354,8 @@ const formatDate = (date: Date): string => {
         <Divider />
         <Container sx={{ textAlign: 'right' }}>
           <Button variant="text">카테고리 설정</Button>
-        </Container>
-        <Container
+          </Container>
+          <Container
           sx={{
             display: 'flex',
             justifyContent: 'space-evenly',
@@ -254,9 +372,8 @@ const formatDate = (date: Date): string => {
                 size="medium"
                 onClick={() => {
                   setSelectedCategoryIdx(idx);
-                }
-                }
-
+                  setSelectedSubCategoryIdx(0);
+                }}
               />
             ) : (
               <Chip
@@ -266,6 +383,7 @@ const formatDate = (date: Date): string => {
                 size="medium"
                 onClick={() => {
                   setSelectedCategoryIdx(idx);
+                  setSelectedSubCategoryIdx(0);
                 }}
               />
             ),
@@ -278,17 +396,15 @@ const formatDate = (date: Date): string => {
             padding: '16px',
           }}
         >
-          {categories &&
-            categories[selectedCategoryIdx].subCategories.map((sub, idx) => (
-              <Circle
+          {categories[selectedCategoryIdx]?.subCategories.map((sub, idx) => (
+              <Chip
                 key={sub.title}
                 label={sub.title}
-                color={sub.color}
-                size={40}
+                variant={selectedSubCategoryIdx === idx ? 'filled' : 'outlined'}
+                size="medium"
                 onClick={() => {
                   setSelectedSubCategoryIdx(idx);
                 }}
-                selected={idx === selectedSubCategoryIdx}
               />
             ))}
         </Container>
@@ -301,9 +417,8 @@ const formatDate = (date: Date): string => {
             fullWidth
           />
         </Container>
-      </Container>
+      </Container>      
     </>
   );
 };
-
-export default CreateRecoredPage;
+export default CreateRecordPage;
--- src/pages/record/RecordPage.tsx
+++ src/pages/record/RecordPage.tsx
@@ -1,10 +1,12 @@
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import FullCalendar from '@fullcalendar/react';
+import dayjs from 'dayjs';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import { Container } from '@mui/system';
 import moment from 'moment-timezone';
-import { useNavigate } from 'react-router-dom';
+import React, { useState,useEffect } from 'react';
+import { useNavigate, useHistory } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
 
 import { SelectedRangeData } from './CreateRecordPage';
@@ -12,6 +14,21 @@ import { SelectedRangeData } from './CreateRecordPage';
 import Spacer from '@/components/common/Spacer';
 import CommonHeader from '@/components/layout/CommonHeader';
 import { selectedTimeRangeState } from '@/store/record';
+
+interface Event {
+  id: number;
+  title: string;
+  start: Date;
+  end: Date;
+}
+
+
+// interface EventInputData {
+//   categoryId: number;
+//   categoryName: string;
+//   startedAt: string;
+//   finishedAt: string;
+// }
 
 const renderEventContent = (eventInfo: any) => {
   return (
@@ -31,8 +48,8 @@ const renderEventContent = (eventInfo: any) => {
 //   {
 //     id: '1',
 //     title: 'test2',
-//     start: '2023-03-21T07:00:00',
-//     end: '2023-03-21T07:30:00',
+//     start: '2023-04-05T07:00:00 KST',
+//     end: '2023-04-05T07:30:00 KST',
 //   },
 // ];
 
@@ -51,32 +68,85 @@ const RecordPage = () => {
     navigation('/record/create');
   };
 
+  // const history = useHistory();
+  // const handleEventClick = (e: any) => {
+  //   history.push(`/record/update?start=${e.start}&end=${e.end}&category=${e.category}`);
+  // }
+
   const handleEventClick = (e: any) => {
     console.log(e);
+    const url = `/record/update/${e.event.id}`;
+    console.log(url);
+    // const url = `/record/update`;
+    // navigation(url);
   };
+
+  // const handleEventClick = (info: EventClickArg) => {
+  //   const event = clickInfo.event;
+
+  //   const { id, title, start, end } = event;
+
+  //   // history.push(`/record/update?id=${id}&title=${title}&start=${start}&end=${end}`);
+  // };
   const handleEvents = (e: any) => {
     console.log(e);
   };
 
+  
+
+  // const [events, setEvents] = useState<Event[]>([]);
+
   ////시간소비 활동 API - 조회
-  // useEffect(() => {
-  //   async function fetchEvents() {
-  //     const res = await fetch('http://localhost:8080/api/v1/activity-records');
-  //     const data = await res.json();
-  //     if (data.length > 0) {
-  //       const events = data.map((item: any) => {
-  //         return {
-  //           id: item.categoryId,
-  //           title: item.categoryName,
-  //           start: new Date(item.startedAt),
-  //           end: new Date(item.finishedAt),
-  //         };
-  //       });
-  //       setEvents(events);
-  //     }
-  //   }
-  //   fetchEvents();
-  // }, []);
+  const member= 1;
+
+  function getWeekStartEndDates(): { fromStartedAt: string, toStartedAt: string } {
+    const now = new Date();
+  
+    // 일주일의 첫 시작일로 설정합니다. 일요일 4시(한국 기준)로 고정합니다.
+    const startOfWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate() - now.getDay(), 4 + 9);
+    // 일주일의 마지막 날짜로 설정합니다. 토요일 3시(한국 기준)로 고정합니다.
+    const endOfWeek = new Date(now.getFullYear(), now.getMonth(), startOfWeek.getDate() + 6, 3 + 9);
+  
+    // 주어진 형식으로 날짜를 문자열로 변환합니다.
+    const fromStartedAt = startOfWeek.toISOString().substring(0, 10) + 'T' + '04:00:00';
+    const toStartedAt = endOfWeek.toISOString().substring(0, 10) + 'T' + '03:00:00';
+  
+    return { fromStartedAt, toStartedAt };
+}
+
+const [events, setEvents] = useState<Event[]>([]);
+const { fromStartedAt, toStartedAt } = getWeekStartEndDates();
+
+useEffect(() => {
+  async function fetchEvents() {
+    try {
+      const res = await fetch(
+        `http://localhost:8080/api/v1/activity-records?memberId=${encodeURIComponent(member)}&fromStartedAt=${encodeURIComponent(fromStartedAt)}&toStartedAt=${encodeURIComponent(toStartedAt)}`
+      );
+      const data = await res.json();
+      console.log(data);
+      if (data.message === "Success") {
+        const events = data.result.map((item: any) => {
+          const startedAt = item.startedAt.replace(" KST", "")
+          const finishedAt = item.startedAt.replace(" KST", "")
+          return {
+            id: item.categoryId,
+            title: item.categoryName,
+            start: startedAt,
+            end: finishedAt,
+            resourceId: item.categoryId,
+          };
+        });
+        setEvents(events);
+        console.log("events: " , events);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  fetchEvents();
+}, [fromStartedAt, toStartedAt]);
+
 
   // const handleRecordCreate = async (info: EventInput) => {
   //   const newEvent = {
@@ -88,22 +158,16 @@ const RecordPage = () => {
   //   setEvents([...events, newEvent]);
   // };
 
-  //로컬에 저장
-  const timezone = 'Asia/Seoul';
-  const initialEvents = (): EventInput[] => {
-  
-  const events = JSON.parse(localStorage.getItem('events') || '[]');
-  return events.map((event) => ({
-    id: event.id.toString(),
-    title: event.title,
-    start:moment.tz(event.start, timezone).format('YYYY-MM-DDTHH:mm:ss'),
-    end: moment.tz(event.end, timezone).format('YYYY-MM-DDTHH:mm:ss'),
-  }));
-};
+  function renderTitle(info: any) {
+    console.log(info.start, info.end);
+    const startDate = dayjs(info.start.marker).day(0).format('M월 D일 (ddd)');
+    const endDate = dayjs(info.start.marker).day(6).format('M월 D일 (ddd)');
+  return `${startDate} ~ ${endDate}`;
+  }
 
   return (
     <div>
-      <CommonHeader title={'메인화면'} />
+      <CommonHeader title={'일주일 기록하기'} />
       <Container
         sx={{
           height: 'calc(100vh - 112px)',
@@ -119,14 +183,20 @@ const RecordPage = () => {
             center: 'title',
             right: 'next',
           }}
+          titleFormat={renderTitle}
           eventColor="#FF8999"
           initialView="timeGridWeek"
           editable={true}
           selectable={true}
           selectMirror={true}
           // dayMaxEvents={true}
-          // initialEvents={initialEvents} // alternatively, use the `events` setting to fetch from a feed
-          events={initialEvents()}
+          // initialEvents={events} // alternatively, use the `events` setting to fetch from a feed
+          // events={initialEvents()}
+          // events={handleRecordCreate}
+          events = {events}
+          eventDidMount={(info) => {
+            console.log('Event did mount:', info.event);
+          }}
           select={handleDateSelect}
           eventContent={renderEventContent} // custom render function
           eventClick={handleEventClick}
