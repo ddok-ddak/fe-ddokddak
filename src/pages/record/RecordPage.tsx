c55c986916c3d69f6f7e26ced06072f8003f0b2b
min <69198709+matchaing@users.noreply.github.com>
2023-04-08 18:25:52 +0900

    일주일 단위 API 조회

--- src/pages/record/RecordPage.tsx
+++ src/pages/record/RecordPage.tsx
@@ -1,10 +1,10 @@
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import FullCalendar from '@fullcalendar/react';
+import { EventInput } from '@fullcalendar/common';
 import dayjs from 'dayjs';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import { Container } from '@mui/system';
-import moment from 'moment-timezone';
 import React, { useState,useEffect } from 'react';
 import { useNavigate, useHistory } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
@@ -15,20 +15,18 @@ import Spacer from '@/components/common/Spacer';
 import CommonHeader from '@/components/layout/CommonHeader';
 import { selectedTimeRangeState } from '@/store/record';
 
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 interface Event {
   id: number;
   title: string;
   start: Date;
   end: Date;
 }
-
-
-// interface EventInputData {
-//   categoryId: number;
-//   categoryName: string;
-//   startedAt: string;
-//   finishedAt: string;
-// }
 
 const renderEventContent = (eventInfo: any) => {
   return (
@@ -92,39 +90,36 @@ const RecordPage = () => {
     console.log(e);
   };
 
-  
-
-  // const [events, setEvents] = useState<Event[]>([]);
-
   ////시간소비 활동 API - 조회
   const member= 1;
-
-  function getWeekStartEndDates(): { fromStartedAt: string, toStartedAt: string } {
-    const now = new Date();
+  const [events, setEvents] = useState<Event[]>([]);
   
-    // 일주일의 첫 시작일로 설정합니다. 일요일 4시(한국 기준)로 고정합니다.
-    const startOfWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate() - now.getDay(), 4 + 9);
-    // 일주일의 마지막 날짜로 설정합니다. 토요일 3시(한국 기준)로 고정합니다.
-    const endOfWeek = new Date(now.getFullYear(), now.getMonth(), startOfWeek.getDate() + 6, 3 + 9);
-  
-    // 주어진 형식으로 날짜를 문자열로 변환합니다.
-    const fromStartedAt = startOfWeek.toISOString().substring(0, 10) + 'T' + '04:00:00';
-    const toStartedAt = endOfWeek.toISOString().substring(0, 10) + 'T' + '03:00:00';
-  
-    return { fromStartedAt, toStartedAt };
-}
+  function renderTitle(info: any) {
+    console.log(info.start, info.end);
+    const startDate = dayjs(info.start.marker).day(0).format('M월 D일 일요일');
+    const endDate = dayjs(info.start.marker).day(6).format('M월 D일 토요일');
+  return `${startDate} ~ ${endDate}`;
+  }
 
-const [events, setEvents] = useState<Event[]>([]);
-const { fromStartedAt, toStartedAt } = getWeekStartEndDates();
 
-useEffect(() => {
-  async function fetchEvents() {
+  useEffect(() => {
+    const initialInfo = { start: new Date() };
+    fetchEvents(initialInfo);
+  }, []);
+  
+  const fetchEvents = async (info: any) => {
+    const fromStartedAt = dayjs(info.start).day(0).hour(4).format('YYYY-MM-DDT04:00:00');
+    const toStartedAt = dayjs(info.start).day(6).hour(3).format('YYYY-MM-DDT03:00:00');
+  
+    console.log('fromStartedAt:', fromStartedAt); // YYYY-MM-DDT04:00:00
+    console.log('toStartedAt:', toStartedAt); // YYYY-MM-DDT03:00:00
+    console.log(fromStartedAt, toStartedAt);
     try {
       const res = await fetch(
         `http://localhost:8080/api/v1/activity-records?memberId=${encodeURIComponent(member)}&fromStartedAt=${encodeURIComponent(fromStartedAt)}&toStartedAt=${encodeURIComponent(toStartedAt)}`
       );
       const data = await res.json();
-      console.log(data);
+  
       if (data.message === "Success") {
         const events = data.result.map((item: any) => {
           const startedAt = item.startedAt.replace(" KST", "")
@@ -137,34 +132,14 @@ useEffect(() => {
             resourceId: item.categoryId,
           };
         });
+  
         setEvents(events);
-        console.log("events: " , events);
+        console.log("events: ", events);
       }
-    } catch (error) {
+    }catch (error) {
       console.error(error);
     }
   }
-  fetchEvents();
-}, [fromStartedAt, toStartedAt]);
-
-
-  // const handleRecordCreate = async (info: EventInput) => {
-  //   const newEvent = {
-  //     id: events.length + 1,
-  //     title: info.categoryName,
-  //     start: info.startedAt,
-  //     end: info.finishedAt,
-  //   };
-  //   setEvents([...events, newEvent]);
-  // };
-
-  function renderTitle(info: any) {
-    console.log(info.start, info.end);
-    const startDate = dayjs(info.start.marker).day(0).format('M월 D일 (ddd)');
-    const endDate = dayjs(info.start.marker).day(6).format('M월 D일 (ddd)');
-  return `${startDate} ~ ${endDate}`;
-  }
-
   return (
     <div>
       <CommonHeader title={'일주일 기록하기'} />
@@ -179,11 +154,15 @@ useEffect(() => {
           allDaySlot={false}
           plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
           headerToolbar={{
-            left: 'prev',
-            center: 'title',
-            right: 'next',
+            left: "prev",
+            center: "title",
+            right: "next",
           }}
           titleFormat={renderTitle}
+          datesSet={(info) => {
+            const title = renderTitle(info);
+            fetchEvents(info);
+          }}
           eventColor="#FF8999"
           initialView="timeGridWeek"
           editable={true}
@@ -191,8 +170,6 @@ useEffect(() => {
           selectMirror={true}
           // dayMaxEvents={true}
           // initialEvents={events} // alternatively, use the `events` setting to fetch from a feed
-          // events={initialEvents()}
-          // events={handleRecordCreate}
           events = {events}
           eventDidMount={(info) => {
             console.log('Event did mount:', info.event);
@@ -211,6 +188,7 @@ useEffect(() => {
           slotMaxTime={'28:00:00'} // 끝시간
           slotDuration={'00:30:00'} // 시간 간격
         />
+
       </Container>
     </div>
   );
