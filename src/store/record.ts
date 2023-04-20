c7f53d4569a58bceee73ef0e2f662075f4fef560
min <69198709+matchaing@users.noreply.github.com>
2023-04-20 21:32:13 +0900

    충돌

--- src/store/record.ts
+++ src/store/record.ts
@@ -6,3 +6,39 @@ export const selectedTimeRangeState = atom({
   key: 'selectedTimeRangeState',
   default: {} as SelectedRangeData,
 });
+
+export const categories = atom<Category[]>({
+  key: "categories",
+  default: [],
+});
+
+export const recoilCategory = atom({
+  key: 'recoilCategory',
+  default: 1,
+});
+
+export const recoilSubCategory = atom({
+  key: 'recoilSubCategory',
+  default: 1,
+});
+
+export const selectedDaysState = atom<number[]>({
+  key: 'selectedDaysState',
+  default: [],
+});
+
+type SubCategory = {
+  name: string;
+  color: string;
+};
+
+type Category = {
+  name: string;
+  color: string;
+  subCategories: SubCategory[];
+};
+
+export const categoriesState = atom<Category[]>({
+  key: 'categoriesState',
+  default: [],
+});
