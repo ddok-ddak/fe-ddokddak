d3755dbfa60010ad1652c9261825d424ba2ead0a
riboha <jek.1976534@gmail.com>
2023-05-24 21:48:09 +0900

    Upt. 통계 화면 차트 구현 (1차)

--- src/pages/statistics/ChartContainer.tsx
+++ src/pages/statistics/ChartContainer.tsx
@@ -15,32 +15,39 @@ import Circle from '@/components/common/Circle';
 import { useRecoilState } from 'recoil';
 import { statisticsResultState } from '@/store/statistics';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { isContext } from 'vm';
 
 ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, ...registerables);
 
-const options = {
-  responsive: true,
-  showTooltips: true,
-  plugins: {
-    legend: {
-      display: false,
-    },
-    datalabels: {
-      formatter: (val: any, ctx: any) => {
-        if (val === 0) {
-          return '';
-        }
-    
-        const hours = Math.floor(val / 60);
-        const minutes = val % 60;
+const hourFormatter = (time: number) => {
+  if (!time) {
+    return '';
+  }
+  const h = Math.floor(time / 60);
+  return h > 0 ? h : 0;
+};
 
-        return minutes === 0 ?
-          `${ctx.chart.data.labels[ctx.dataIndex]}\n${hours}시간` :
-          `${ctx.chart.data.labels[ctx.dataIndex]}\n${hours}시간 ${minutes}분`;
-      },
-    },
+const minuteFormatter = (time: number) => {
+  if (!time) {
+    return '';
+  }
+  const m = Math.floor(time % 60);
+  return m > 0 ? m : 0;
+};
+
+const customBackground = {
+  id: 'customBackgroundColor',
+  beforeDraw: (chart: any, args: any, options: any) => {
+    const { ctx } = chart;
+    if (!ctx) {
+      return true;
+    }
+    ctx.save();
+    ctx.globalCompositeOperation = 'destination-over';
+    ctx.fillStyle = options.color;
+    ctx.fillRect(0, 0, chart.width, chart.height);
+    ctx.restore();
   },
-  showToolTip: true,
 };
 
 const ChartContainer = () => {
@@ -48,25 +55,193 @@ const ChartContainer = () => {
     statisticsResultState,
   );
   const [totalSum, setTotalSum] = useState(0);
-  // const [chartData, setChartData] = useState()
+  const [showTotalSum, setShowTotalSum] = useState(true);
 
   const carouselOption = {
     autoPlay: false,
-    // animation: 'slide',
     duration: 0,
     fullHeightHoverWrapper: {
       height: '100%',
       top: '0',
     },
+    indicatorContainerProps: {
+      style: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    },
+    indicatorIconButtonProps: {
+      style: {
+        transform: 'scale(0.8)',
+        padding: '3px',
+        color: '#FFDCE1',
+      },
+    },
+    activeIndicatorIconButtonProps: {
+      style: {
+        color: '#FF7184',
+      },
+    },
   };
 
   const pieChartRef = useRef<ChartJS>(null);
+  const barChartRef = useRef<ChartJS>(null);
+
   const handleClickPieChart = (event: MouseEvent<HTMLCanvasElement>) => {
     const { current: chart } = pieChartRef;
-
     if (!chart) {
       return;
     }
+    const target = getElementsAtEvent(chart, event);
+    setShowTotalSum(!target.length);
+
+    // TODO: show done list of the selected category below
+  };
+
+  const handleClickBarChart = (event: MouseEvent<HTMLCanvasElement>) => {
+    const { current: chart } = pieChartRef;
+    if (!chart) {
+      return;
+    }
+
+    // TODO: show done list of the selected category below
+  };
+
+  const resultData = [...statisticsResult.filter((data) => data.timeSum > 0)];
+  const chartData = {
+    labels: resultData.map((data) => data.categoryName),
+    datasets: [
+      {
+        data: resultData.map((data) => data.timeSum),
+        backgroundColor: resultData.map((data) => `${data.categoryColor}`),
+        borderWidth: 0,
+      },
+    ],
+  };
+
+  const pieChartOptions = {
+    layout: {
+      padding: {
+        top: 36,
+        bottom: 25,
+      },
+    },
+    responsive: true,
+    showTooltips: true,
+    hoverOffset: 25,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      datalabels: {
+        color: '#FFFFFF',
+        labels: {
+          title: {
+            formatter: (val: any, ctx: any) => {
+              return `${ctx.chart.data.labels[ctx.dataIndex]}\n`;
+            },
+            font: {
+              weight: 'bold',
+              size: 17,
+            },
+          },
+          value: {
+            formatter: (val: any) => {
+              if (!totalSum) {
+                return '';
+              }
+              return `\n${Math.round((val * 100) / totalSum)}%`;
+            },
+            font: {
+              size: 14,
+            },
+          },
+        },
+      },
+      customBackgroundColor: {
+        color: '#FFF4F6',
+      },
+    },
+  };
+
+  const barChartOptions = {
+    layout: {
+      padding: {
+        top: 80,
+        bottom: 15,
+        left: 0,
+        right: 0,
+      },
+    },
+    responsive: true,
+    showTooltips: true,
+    hoverOffset: 25,
+    aspectRatio: 1,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      datalabels: {
+        anchor: 'end',
+        color: (context: any) => {
+          return context.clicked ? 'blue' : '#B7B7B7';
+        },
+        labels: {
+          value: {
+            formatter: (val: any) => {
+              const hours = Math.floor(val / 60);
+              const minutes = val % 60;
+              return `${hours}시간\n ${minutes}분`;
+            },
+            font: {
+              weight: 'bold',
+              size: 14,
+            },
+          },
+        },
+      },
+      customBackgroundColor: {
+        color: '#FFF4F6',
+      },
+    },
+    barThickness: 'flex',
+    maxBarThickness: 20,
+    borderRadius: {
+      topLeft: 20,
+      topRight: 20,
+      bottomRight: 20,
+      bottomLeft: 20,
+    },
+    borderSkipped: false,
+    scales: {
+      x: {
+        ticks: {
+          color: '#222222',
+          backdropColor: 'black',
+          font: {
+            size: 14,
+            weight: 'bold',
+          },
+        },
+        border: {
+          display: false,
+        },
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        border: {
+          display: false,
+        },
+        grid: {
+          display: false,
+        },
+        display: false,
+        min: -5,
+      },
+    },
   };
 
   useEffect(() => {
@@ -79,117 +254,168 @@ const ChartContainer = () => {
 
   return (
     <>
-      {/* <Carousel {...carouselOption}> */}
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          padding: '20px',
-          position: 'relative',
-        }}
-      >
-        <Box
-          sx={{
-            position: 'absolute',
-            top: '45%',
-            textAlign: 'center',
-            color: '#FF7184',
-            fontSize: '16px',
-            fontWeight: '700',
-          }}
-        >
-          총 <br />
-          {totalSum % 60 === 0 ? 
-            `${Math.floor(totalSum / 60)}시간` :
-            `${Math.floor(totalSum / 60)}시간 ${totalSum % 60}분`
-          }
-        </Box>
-        <Chart
-          ref={pieChartRef}
-          type={'doughnut'}
-          data={{
-            labels: [...statisticsResult.map((data) => data.categoryName)],
-            datasets: [
-              {
-                data: [...statisticsResult.map((data) => data.timeSum )],
-                backgroundColor: [
-                  ...statisticsResult.map((data) => `${data.categoryColor}45`),
-                ],
-                borderColor: [
-                  ...statisticsResult.map((data) => `${data.categoryColor}`),
-                ],
-                borderWidth: 1,
-              },
-            ],
-          }}
-          options={options}
-          onClick={handleClickPieChart}
-          plugins={[ChartDataLabels]}
-        />
-      </Box>
-      {/* <Box
-          sx={{ height: '400px', display: 'flex', justifyContent: 'center' }}
-        >
-          <Chart
-            ref={barChartRef}
-            type={'bar'}
-            data={barData}
-            options={options}
-            onClick={handleClickBarChart}
-          />
-        </Box> */}
-      {/* </Carousel> */}
-
-      {statisticsResult.map((data, idx) => (
-        <Box
-          key={idx}
-          sx={{
-            padding: '8px',
-            display: 'flex',
-            justifyContent: 'flex-start',
-            marginTop: '8px',
-            marginLeft: '12px',
-            alignItems: 'flex-end',
-          }}
-        >
-          <Circle color={data.categoryColor} size={40} />
-          <Box sx={{ width: '60%', margin: '0px 10px 0px 10px' }}>
-            <Typography>{data.categoryName}</Typography>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <Box sx={{ width: '100%', mr: 1 }}>
-                <LinearProgress
-                  variant="determinate"
-                  value={data.timeSum }
-                  sx={{
-                    height: 10,
-                    borderRadius: 5,
-                    '&.MuiLinearProgress-root': {
-                      backgroundColor: '#F5F5F5 !important',
-                    },
-                    '& > .MuiLinearProgress-bar': {
-                      backgroundColor: data.categoryColor,
-                    },
-                  }}
-                />
-              </Box>
-              <Box sx={{ minWidth: 35 }}>
-              <Typography
-                variant="body2"
-                color="text.secondary"
+      {
+        <Carousel {...carouselOption}>
+          <Box
+            sx={{
+              display: 'flex',
+              height: '274px',
+              justifyContent: 'center',
+              position: 'relative',
+              width: '100%',
+              backgroundColor: '#FFF4F6',
+            }}
+          >
+            <Box
+              sx={{
+                display: showTotalSum ? '' : 'none',
+                position: 'absolute',
+                height: '274px',
+                top: '45%',
+                textAlign: 'center',
+                color: '#FF7184',
+                fontSize: '16px',
+                fontWeight: '700',
+              }}
+            >
+              <span>총 </span>
+              <span>{hourFormatter(totalSum)}</span>
+              <span> 시간 </span>
+              <span>{minuteFormatter(totalSum)}</span>
+              <span> 분</span>
+            </Box>
+            <Chart
+              ref={pieChartRef}
+              type={'doughnut'}
+              data={chartData}
+              options={pieChartOptions}
+              onClick={handleClickPieChart}
+              plugins={[ChartDataLabels, customBackground]}
+            />
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: '#FFF4F6',
+            }}
+          >
+            <Box
+              sx={{
+                position: 'absolute',
+                top: 0,
+                width: '100%',
+                height: '57px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderBottom: '3px solid white',
+                color: '#222222',
+                fontWeight: 'bold',
+                fontSize: '14px',
+              }}
+            >
+              <span>총 </span>
+              <span
+                style={{
+                  fontSize: '16px',
+                  color: '#FF7184',
+                }}
               >
-                {`${totalSum > 0 ? Math.round(((data.timeSum) / totalSum) * 100) : 0}%`}
-              </Typography>
-              </Box>
+                {hourFormatter(totalSum)}
+              </span>
+              <span> 시간 </span>
+              <span
+                style={{
+                  fontSize: '16px',
+                  color: '#FF7184',
+                }}
+              >
+                {minuteFormatter(totalSum)}
+              </span>
+              <span> 분</span>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                height: '274px',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                width: '100%',
+              }}
+            >
+              <Chart
+                ref={barChartRef}
+                type={'bar'}
+                data={chartData}
+                options={barChartOptions}
+                onClick={handleClickBarChart}
+                plugins={[ChartDataLabels, customBackground]}
+              />
             </Box>
           </Box>
-          <Typography>
-            {data.timeSum % 60 === 0 ? 
-            `${Math.floor(data.timeSum / 60)}시간` :
-            `${Math.floor(data.timeSum / 60)}시간 ${data.timeSum % 60}분`
-            }
-          </Typography>
-        </Box>
-      ))}
+        </Carousel>
+      }
+
+      {statisticsResult
+        .filter((data) => data.timeSum > 0)
+        .map((data, idx) => (
+          <Box
+            key={idx}
+            sx={{
+              padding: '8px',
+              display: 'flex',
+              justifyContent: 'flex-start',
+              marginTop: '8px',
+              marginLeft: '12px',
+              alignItems: 'flex-end',
+            }}
+          >
+            <Circle color={data.categoryColor} size={40} />
+            <Box sx={{ width: '60%', margin: '0px 10px 0px 10px' }}>
+              <Typography>{data.categoryName}</Typography>
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Box sx={{ width: '100%', mr: 1 }}>
+                  <LinearProgress
+                    variant="determinate"
+                    value={data.timeSum}
+                    sx={{
+                      height: 10,
+                      //borderRadius: 5,
+                      '&.MuiLinearProgress-root': {
+                        backgroundColor: '#F5F5F5 !important',
+                      },
+                      '& > .MuiLinearProgress-bar': {
+                        backgroundColor: data.categoryColor,
+                      },
+                    }}
+                  />
+                </Box>
+                {/* <Box sx={{ minWidth: 35 }}>
+                <Typography variant="body2" color="text.secondary">
+                  {`${
+                    totalSum > 0
+                      ? Math.round((data.timeSum / totalSum) * 100)
+                      : 0
+                  }%`}
+                </Typography>
+              </Box> */}
+              </Box>
+            </Box>
+            <Typography
+              sx={{
+                display: 'flex',
+                alignItems: 'end',
+                justifyContent: 'center',
+                flex: '10% 1 1',
+                fontSize: '0.8rem',
+              }}
+            >
+              {data.timeSum % 60 === 0
+                ? `${Math.floor(data.timeSum / 60)}시간`
+                : `${Math.floor(data.timeSum / 60)}시간 ${data.timeSum % 60}분`}
+            </Typography>
+          </Box>
+        ))}
     </>
   );
 };
