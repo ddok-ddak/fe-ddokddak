8881a4f66e0ad905009a988db5cba4be8f4867d6
chobani <jek.1976534@gmail.com>
2023-10-06 14:33:52 +0900

    Upt. Mui Custom Style Theme Color Code Update

--- src/pages/statistics/Period.tsx
+++ src/pages/statistics/Period.tsx
@@ -18,21 +18,21 @@ import {
   AppBar,
   Box,
   Grid,
+  IconButton,
+  InputAdornment,
   styled,
   Tab,
-  Tabs
+  Tabs,
+  TextField
 } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import dayjs from 'dayjs';
+import dayjs, { ManipulateType } from 'dayjs';
 import 'dayjs/locale/ko';
-import isBetweenPlugin from 'dayjs/plugin/isBetween';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 import utc from 'dayjs/plugin/utc';
-import weekdayPlugin from 'dayjs/plugin/weekday';
 import { SyntheticEvent, useEffect } from 'react';
 import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
-import { useStatisticView } from '@/hooks/statisticView';
 
 dayjs.extend(utc);
 dayjs.extend(localizedFormat);
@@ -55,7 +55,49 @@ interface StyledTabProps {
 }
 
 /**
- * custom styled period select tab group
+ * render date input
+ */
+export const renderDateInput = ({params, width}: any) => {
+  return (
+    <TextField 
+      {...params}
+      helperText={null}
+      InputProps={{
+        endAdornment: 
+          <InputAdornment position="end" sx={{m: 0}}>
+            <IconButton>
+              <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path 
+                  fillRule="evenodd" 
+                  clipRule="evenodd" 
+                  d="M3.52941 0H4.41176V1.76471H10.5882V0H11.4706V1.76471H14H15V2.76471V13.1176V14.1176H14H1H0V13.1176V2.76471V1.76471H1H3.52941V0ZM10.5882 2.76471V3.52941H11.4706V2.76471H14V6.17647H1V2.76471H3.52941V3.52941H4.41176V2.76471H10.5882ZM1 7.05882V13.1176H14V7.05882H1Z" 
+                  fill="#4B4B4B"
+                />
+              </svg>
+            </IconButton>
+          </InputAdornment>
+      }}
+      sx={{
+        display: 'flex',
+        ' .MuiInputBase-root': { justifyContent: 'center', p: 0 },
+        ' .MuiInputBase-input': { 
+          p: '16px 0',
+          flex: '1 1 auto',
+          overflow: 'auto',
+          color: '#4B4B4B',
+          fontWeight: '600',
+          fontSize: '14px',
+          width: width + 'px',
+        },
+        ' .MuiInputAdornment-root' : {flex: '0 0 auto', pb: '1px'},
+        '& input': { pl: 0 },
+        '& fieldset': { border: 'none' }
+      }}
+    />
+)};
+
+/**
+ * custom styled tab group
  */
 const StyledTabs = styled((props: StyledTabProps) => (
   <Tabs
@@ -113,21 +155,11 @@ const Period = () => {
     statisticsResultState,
   );
 
-  const { getWeekPeriodInputFormat, setNewDateRange } 
-    = useStatisticView();
-
-
-  /**
-   * get data with selected date
-   * @param request
-   * @param request
-   */
-  const getStatistics = async (request: StatisticsRequest) => {
-    const response = await getStatisticsData({
-      ...request,
-    });
-    setStatisticsResult(response.result);
+  const handlePeriodChange = (e: SyntheticEvent, selectedDate: PeriodType) => {
+    setPeriodType(selectedDate);
   };
+
+
 
   const getPeriodString = (period: PeriodType): ManipulateType | undefined => {
     switch (period) {
@@ -142,6 +174,27 @@ const Period = () => {
       default:
         break;
     }
+  };
+
+  /**
+   * set data search range with newly selected date
+   * @param newValue newly selected date
+   */
+  const setNewDateRange = (newValue: any) => {
+    if (newValue) {
+      setSelectedDate({...selectedDate, [periodType]: newValue});
+    }
+  };
+
+  /**
+   * get data with selected date
+   * @param request 
+   */
+  const getStatistics = async (request: StatisticsRequest) => {
+    const response = await getStatisticsData({
+      ...request,
+    });
+    setStatisticsResult(response.result);
   };
 
 
