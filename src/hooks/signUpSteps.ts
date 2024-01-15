7a55f12a1da91763a67d19f44ccee54d95d3a577
choba <jek.1976534@gmail.com>
2024-01-15 13:26:15 +0100

    Add. SignUp / ResetPW Bug

--- src/hooks/signUpSteps.ts
+++ src/hooks/signUpSteps.ts
@@ -1,17 +1,18 @@
-import { signUpStep } from '@/store/signUp';
+import { stepIndex } from '@/store/common';
 import { ReactElement } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 
 export function useSignUpStepForm(steps: ReactElement[]) {
 
-  const [currentStepIndex, setCurrentStepIndex] = useRecoilState(signUpStep);
+  const [currentStepIndex, setCurrentStepIndex] = useRecoilState(stepIndex);
   const navigate = useNavigate();
 
   /**
    * handle next button
    */
   function handleNextButton() {
+
     const currIdx = currentStepIndex;
     setCurrentStepIndex(() => (currIdx < steps.length - 1) ? (currIdx + 1) : currIdx);
   }
