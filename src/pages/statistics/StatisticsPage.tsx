5c9b8a71c135e061cc09daaaf3a2a917f48549a8
choba <jek.1976534@gmail.com>
2024-03-04 09:16:02 +0900

    Del. remove comments

--- src/pages/statistics/StatisticsPage.tsx
+++ src/pages/statistics/StatisticsPage.tsx
@@ -2,20 +2,11 @@ import Period from './Period';
 
 import CommonHeader from '@/components/layout/CommonHeader';
 import ChartContainer from '@/pages/statistics/ChartContainer';
-// import { useRecoilState } from 'recoil';
-// import { modalState } from '@/store/modal';
 
 const StatisticsPage = () => {
-  // const [modalInfo, setModalInfo] = useRecoilState(modalState);
-  // const handleClickBtn = () => {
-  //   setModalInfo({ ...modalInfo, open: true, msg: 'This is a test.' });
-  // };
-
   return (
     <>
       <CommonHeader title={'통계'} isShowPrevButton={false} />
-      {/* <button onClick={handleClickBtn}>POPUP TEST</button> */}
-
       <Period />
       <ChartContainer />
     </>
